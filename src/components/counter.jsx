5962d51287b0fb164618c6bc4bc1becd1065e1fa
davit <datokikvadze8@gmail.com>
2022-10-30 00:36:01 +0400

    add usereducer

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import { useReducer } from "react";
 import Navigation from "./navigation";
 import styled from "styled-components";
 
 const Counter = () => {
+    const initialState = { counter: 5 };
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "increase": {
+                return { ...state, counter: state.counter + action.payload };
+            }
+            case "decrease": {
+                return { ...state, counter: state.counter - action.payload };
+            }
+            default:
+                throw new Error();
+        }
+    };
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
     return (
         <Container>
             <Navigation />
             <div className="counter-wrapper">
-                <h1>10</h1>
+                <h1>{state.counter}</h1>
                 <div className="buttons-wrapper">
-                    <button>plus</button>
-                    <button>minus</button>
+                    <button onClick={() => dispatch({ type: "increase", payload: 5 })}>plus</button>
+                    <button onClick={() => dispatch({ type: "decrease", payload: 5 })}>
+                        minus
+                    </button>
                 </div>
             </div>
         </Container>
